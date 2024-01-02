81f42a6004d9f3c7f4b011421a67be55197fb48d
Ferkox <fernandogonzalezgaete@gmail.com>
2024-01-02 18:48:42 -0300

    Update routing modules and components

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
-import { HeroPageComponent } from './page/hero-page/hero-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     HeroPageComponent,
     ListPageComponent,
     NewPageComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    LayoutPageComponent
   ],
   imports: [
     CommonModule,
