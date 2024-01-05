9b04310f12b59d101fb6c00191bacb8e7c50c6ef
Fernando González Gaete <83770169+FerkoxGG@users.noreply.github.com>
2024-01-05 03:07:39 -0300

    Update to get heroes from API

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
-  constructor() { }
+  private baseUrl: string = environment.baseUrl
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getHero(): Observable<Hero[]> {
+    return this.httpClient.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
 }
