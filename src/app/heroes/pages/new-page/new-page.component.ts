d47621306abf64f9949370b833b07ac7d5f6e7f8
Fernando González Gaete <83770169+FerkoxGG@users.noreply.github.com>
2024-01-14 01:27:55 -0300

    Update component to support editing and deleting heroes

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,36 @@
-import { Component, inject } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Component, OnInit, inject } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
 import { Hero, Publisher } from "../../interfaces/hero.interface";
 import { HeroesService } from "../../services/heroes.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { switchMap } from "rxjs";
+import { MatDialog } from "@angular/material/dialog";
 
 @Component({
   selector: "app-new-page",
   templateUrl: "./new-page.component.html",
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+  private _snackBar = inject(MatSnackBar);
+  private activatedRoute = inject(ActivatedRoute);
+  private heroesService = inject(HeroesService);
+  private router = inject(Router);
+  private dialog = inject(MatDialog);
+
+  ngOnInit(): void {
+    if (!this.router.url.includes("edit")) return;
+
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe(hero => {
+        if (!hero) return this.router.navigateByUrl("/");
+
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
   public heroForm = new FormGroup({
     id: new FormControl<string>(""),
     superhero: new FormControl<string>("", { nonNullable: true }),
@@ -24,32 +46,50 @@ export class NewPageComponent {
     { id: "Marvel Comics", value: "Marvel - Comics" }
   ];
 
-  private heroesService = inject(HeroesService);
-
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
 
     return hero;
   }
 
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
 
-  onSubmit():void {
-    if (this.heroForm.valid) return;
-
-    if ( this.currentHero.id ) {
-      this.heroesService.updateHero(this.currentHero)
-      .subscribe(hero => {
-        //TODO: mostrar snackbar
-        alert(`The hero ${hero.superhero} has been updated!`);
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe(hero => {
+        this.showSnackBar(
+          `El héroe ${hero.superhero} se actualizó correctamente!`
+        );
       });
 
       return;
     }
 
-    this.heroesService.addHero(this.currentHero)
-    .subscribe(() => {
-      //TODO: mostrar snackbar y navegar a /heroes/edit/ hero.id
-    })
+    this.heroesService.addHero(this.currentHero).subscribe(hero => {
+      this.router.navigate(["/heroes/edit", hero.id]);
+      this.showSnackBar(
+        `El héroe ${hero.superhero} se ha creado correctamente!`
+      );
+    });
     // this.heroesService.updateHero(this.heroForm.value).
+  }
+
+  onDelete(): void {
+    if (
+      !confirm(
+        `Estás seguro/a de eliminar al héroe "${this.currentHero.superhero}"?`
+      )
+    )
+      return;
+    this.heroesService.deleteHeroById(this.currentHero).subscribe(() => {
+      this.showSnackBar(`El héroe ha sido eliminado`);
+      this.router.navigate(["/heroes"]);
+    });
+  }
+
+  showSnackBar(message: string): void {
+    this._snackBar.open(message, "done", {
+      duration: 2000
+    });
   }
 }
